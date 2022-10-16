7a520ce7041e2b13be245d481dbafb1f2b017ac4
fabxy <fwaschkowski@student.unimelb.edu.au>
2022-10-16 21:51:08 +0200

    constraints, data v1

--- run_dso.py
+++ run_dso.py
@@ -1,6 +1,8 @@
 """XYZ"""
 
 import os
+import numpy as np
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -21,8 +23,6 @@ import torch.optim as optim
 # - Get data
 # - Device
 
-# torch.manual_seed(0)
-
 
 class Symbol(object):
     """XYZ"""
@@ -32,7 +32,7 @@ class Symbol(object):
         self.name = name
         self.arity = arity
 
-    def __repr__(self):                 # TODO: Do we want this?
+    def __str__(self):
         return self.name
 
 
@@ -43,44 +43,90 @@ class Expression(object):
         
         self.symbols = []
         self.likelihood = 1.0
+
         self.open_syms = 1
+        self.par_sym = None
+        self.sib_sym = None
+
+    def __getitem__(self, val):
+        return self.symbols[val]
+
+    def __len__(self):
+        return len(self.symbols)
 
     def add(self, sym, prob):
 
         self.symbols.append(sym)
         self.likelihood *= prob
+        
         self.open_syms += sym.arity - 1
+        self.update_status()
 
-    def get_status(self):
+        return self.get_status()
+
+    def update_status(self):
 
         if self.open_syms == 0:
-            par_sym = None
-            sib_sym = None
+            self.par_sym = None
+            self.sib_sym = None
 
         else:
             sym = self.symbols[-1]
             if sym.arity > 0:
-                par_sym = sym
-                sib_sym = None
-            
+                self.par_sym = sym
+                self.sib_sym = None
+
             else:
-                c = -1
-                i = len(self.symbols) - 1
+                counter = -1
+                idx = len(self.symbols) - 1
                 
-                while c != 0:
-                    i -= 1
-                    c += self.symbols[i].arity - 1
+                while counter != 0:
+                    idx -= 1
+                    counter += self.symbols[idx].arity - 1
 
-                par_sym = self.symbols[i]
-                sib_sym = self.symbols[i+1]
+                self.par_sym = self.symbols[idx]
+                self.sib_sym = self.symbols[idx+1]
             
-        return self.open_syms, par_sym, sib_sym
+    def get_status(self):
+
+        return self.open_syms, self.par_sym, self.sib_sym
 
 
-class DSOnet(nn.Module):
+class Constraints(object):
     """XYZ"""
 
-    def __init__(self, pool, hid_size):
+    def __init__(self, max_len=None):
+
+        self.max_len = max_len
+               
+    def __call__(self, pred, expr, pool):
+
+        if self.max_len is not None:
+
+            open_syms, _, _ = expr.get_status()
+
+            max_arity = self.max_len - len(expr) - open_syms
+
+            for s, sym in enumerate(pool):
+                if sym.arity > max_arity: pred[s] = 0.0
+
+        pred = pred / pred.sum()
+                
+        return pred
+
+
+# class Reward(object):
+#     """XYZ"""
+
+#     def __init__(self, objective="NRMSE"):
+
+#         self.objective = objective
+            
+
+class DSO(nn.Module):
+    """XYZ"""
+
+    def __init__(self, pool, hid_size, constraints=None):
         super().__init__()
 
         self.pool = pool
@@ -93,10 +139,8 @@ class DSOnet(nn.Module):
         
         self.linear = nn.Linear(hid_size, self.out_size)
 
-        # max sequence length
-        # hidden size same as pool length?
-
-    
+        self.constraints = constraints
+          
     def get_input(self, par_sym=None, sib_sym=None):
 
         in_data = torch.zeros(self.in_size)                     # TODO: explicit empty symbol encoding?
@@ -109,52 +153,60 @@ class DSOnet(nn.Module):
 
         return in_data
 
-
-    def get_symbol(self, in_data, state=None):
+    def get_probabilities(self, in_data, state=None):
 
         h, c = self.rnn(in_data, state)
+        out = self.linear(h)
+        probs = F.softmax(out, dim=0)
 
-        probs = F.softmax(self.linear(h), dim=0)
+        return probs, (h, c)
 
+    def get_symbol(self, probs):
+        
         sym_idx = torch.multinomial(probs, 1)
 
-        return self.pool[sym_idx], probs[sym_idx], (h, c)
-
+        return self.pool[sym_idx], probs[sym_idx]
 
     def forward(self):
 
         expr = Expression()
 
+        # get first symbol
         in_data = self.get_input()
-        sym, prob, state = self.get_symbol(in_data)
+        probs, state = self.get_probabilities(in_data)
 
-        expr.add(sym, prob)
-        open_syms, par_sym, sib_sym = expr.get_status()
+        sym, prob = self.get_symbol(probs)
+        open_syms, par_sym, sib_sym = expr.add(sym, prob)
 
+        # get remaining symbols
         while open_syms:
 
             in_data = self.get_input(par_sym, sib_sym)
-            sym, prob, state = self.get_symbol(in_data, state)
+            probs, state = self.get_probabilities(in_data, state)
 
-            expr.add(sym, prob)
-            open_syms, par_sym, sib_sym = expr.get_status()
+            if self.constraints:
+                probs = self.constraints(probs, expr, self.pool)
+            
+            sym, prob = self.get_symbol(probs)
+            open_syms, par_sym, sib_sym = expr.add(sym, prob)
 
         return expr
-
-        # initialize RNN cell state to zero
-        # run cell
-        # get probs
-        # apply constraints
-        # apply softmax
-        # sample symbol
-        # add to expression
-        # finish or get next parent and sibling
 
         # make batches work?!
 
 
 if __name__ == "__main__":
 
+    torch.manual_seed(1)
+
+    # define hyperparameters
+    hid_size = 8
+    max_len = 20
+
+    epochs = 10
+    batch_size = 20
+
+    # define symbol pool
     plus = Symbol("+", 2)
     minus = Symbol("-", 2)
     times = Symbol("*", 2)
@@ -164,8 +216,31 @@ if __name__ == "__main__":
 
     pool = [plus, minus, times, x_sym, y_sym]
 
-    model = DSOnet(pool, 6)
+    # define constraints
+    constraints = Constraints(max_len=max_len)
 
-    expr = model()
+    # create model
+    # TODO: hyperparam dict, loading, saving
+    model = DSO(pool=pool, hid_size=hid_size, constraints=constraints)
 
-    print([s.name for s in expr.symbols])
+    # load data
+    data_path = "data"
+    data_name = "Nguyen-1"
+    data_ext = "csv"
+
+    data = np.loadtxt(os.path.join(data_path, f"{data_name}.{data_ext}"), delimiter=',')
+
+    X_train = torch.Tensor(data[:,:-1])
+    y_train = torch.Tensor(data[:,-1])
+
+    # define optimizer
+
+    # define reward function
+
+    # run training
+    for epoch in range(epochs):
+
+        batch = [model() for _ in range(batch_size)]
+
+        for expr in batch:
+            print([str(s) for s in expr.symbols])
